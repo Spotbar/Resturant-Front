adc1eb3c42420ed89257f7dc68777f0e7e3173fc
elham shahri <elhamshahri2359@gmail.com>
2023-05-21 13:56:38 +0330

    customize and RTL mui x DatePicker

--- src/components/DatePickerX/JalaliDatepicker.tsx
+++ src/components/DatePickerX/JalaliDatepicker.tsx
@@ -4,12 +4,28 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import useTheme from "@mui/system/useTheme";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { faIR as coreFaIR } from "@mui/material/locale";
+import { faIR } from "@mui/x-date-pickers/locales";
 
 export default function JalaliDatepicker() {
   const existingTheme = useTheme();
-  const theme = React.useMemo(
-    () => createTheme({ direction: "rtl" }, existingTheme),
-    [existingTheme]
+  // const theme = React.useMemo(
+  //   () => createTheme({ direction: "rtl" }, existingTheme),
+  //   [existingTheme]
+  // );
+
+  const theme = createTheme(
+    {
+      palette: {
+        primary: { main: "#d97706" },
+      },
+      direction: "rtl",
+      typography: {
+        fontFamily: "IRANSansMobile",
+      },
+    },
+    coreFaIR, // x-date-pickers translations
+    faIR // core translations
   );
 
   return (
@@ -25,7 +41,7 @@ export default function JalaliDatepicker() {
           }}
         >
           <DatePicker
-            label="Date Picker"
+            // label="Date Picker"
             defaultValue={new Date(2022, 1, 1)}
           />
         </LocalizationProvider>
